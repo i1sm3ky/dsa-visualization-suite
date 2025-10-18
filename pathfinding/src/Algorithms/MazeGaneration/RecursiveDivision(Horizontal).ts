d0a831b298e1ec67fc479d941b43fe50d78f5f86
i1sm3ky <kunal21000@gmail.com>
2025-10-18 19:53:51 +0530

    fix

--- pathfinding/src/Algorithms/MazeGaneration/RecursiveDivision(Horizontal).ts
+++ pathfinding/src/Algorithms/MazeGaneration/RecursiveDivision(Horizontal).ts
@@ -2,7 +2,7 @@ import { useStateStore } from "../../store";
 import { animateMaze } from "./utils";
 import { recursiveDivision } from "./RecursiveDivision";
 
-function chooseOrientation(height: number, width: number) {
+function chooseOrientation() {
   return Math.random() < 0.75 ? "H" : "V";
 }
 
@@ -10,7 +10,7 @@ export async function RecursiveDivisionHorizontal() {
   const { gridDim, setDisableButtons } = useStateStore.getState();
   const steps: { row: number; col: number }[] = [];
 
-  recursiveDivision(0, gridDim[0] - 1, 0, gridDim[1] - 1, chooseOrientation(gridDim[0], gridDim[1]), steps, chooseOrientation);
+  recursiveDivision(0, gridDim[0] - 1, 0, gridDim[1] - 1, chooseOrientation(), steps, chooseOrientation);
 
   await animateMaze(steps);
   setDisableButtons(false);
